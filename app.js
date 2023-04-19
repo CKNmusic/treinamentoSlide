b393bf70a40bc6b2b96a27c1c43ba7fb99131c78
CKNmusic <57720235+CKNmusic@users.noreply.github.com>
2023-04-19 17:26:01 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.on('image change', function(image) {
+        io.emit('image change', image);
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
