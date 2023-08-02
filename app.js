21de527582e91ffb0402846ad4e9aa36f6aaab70
CKNmusic <57720235+CKNmusic@users.noreply.github.com>
2023-08-02 13:37:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+let currentSlide = 1; // Número inicial do slide
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -11,11 +13,20 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+    
+    // Enviar o número do slide atual para o cliente
+    socket.emit('current slide', currentSlide);
+    
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
     socket.on('image change', function(image) {
         console.log('image change: ' + image);
+        
+        // Atualizar o número do slide quando a imagem mudar
+        currentSlide = image;
+        
+        // Enviar a alteração do slide para todos os clientes conectados
         io.emit('image change', image);
     });
 });
